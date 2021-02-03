af6bf0e7775125a293d962e4f8e3284890eac52c
Vladislav Leonov <v_leonov1@mail.ru>
2021-02-03 09:01:42 +0300

    fixes homework-4 menu

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -2,23 +2,25 @@ import cn from 'classnames';
 import s from './index.module.css';
 
 function Menu ({isActive}) {
+  const menu = [
+    {title: 'HOME', url: '#welcome'},
+    {title: 'GAME', url: '#game'},
+    {title: 'ABOUT', url: '#about'},
+    {title: 'CONTACT', url: '#contact'},
+  ];
+
   return (
-    <div className={cn(s.menuContainer, {[s.active]: isActive}, s.deactive)}>
+    <div className={cn(s.menuContainer, {[s.active]: isActive, [s.deactive]: !isActive})}>
       <div className={s.overlay} />
       <div className={s.menuItems}>
         <ul>
-          <li>
-            <a href="#welcome">HOME</a>
-          </li>
-          <li>
-            <a href="#game">GAME</a>
-          </li>
-          <li>
-            <a href="#about">ABOUT</a>
-          </li>
-          <li>
-            <a href="#contact">CONTACT</a>
-          </li>
+          {
+            menu.map(item => (
+              <li>
+                <a href={item.url}>{item.title}</a>
+              </li>
+            ))
+          }
         </ul>
       </div>
     </div>
