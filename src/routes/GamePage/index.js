7192a32f276589f7697d20709d1f54a724757c57
Vladislav Leonov <v_leonov1@mail.ru>
2021-02-09 09:29:57 +0300

    homework-6 fixes game page

--- src/routes/GamePage/index.js
+++ src/routes/GamePage/index.js
@@ -3,17 +3,43 @@ import { useHistory } from 'react-router-dom';
 
 import PokemonCard from '../../components/PokemonCard';
 
-import {database, fire} from '../../service/firebase';
+import {database} from '../../service/firebase';
 
 import s from './index.module.css';
+
+const DATA = {
+  abilities : [ "keen-eye", "tangled-feet", "big-pecks" ],
+  base_experience : 122,
+  height : 11,
+  id : 491,
+  img : "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/17.png",
+  name : "pidgeotto",
+  stats : {
+    attack : 60,
+    defense : 55,
+    hp : 63,
+    'special-attack' : 50,
+    'special-defense' : 50,
+    speed : 71
+  },
+  type : "flying",
+  values : {
+    bottom : 7,
+    left : 5,
+    right : 2,
+    top : 'A'
+  }
+};
 
 function  GamePage () {
   const history = useHistory();
   const [pokemons, setPokemons] = useState([]);
 
-  useEffect(getPokemonCards, []);
+  useEffect(() => {
+    getPokemons();
+  }, []);
   
-  function getPokemonCards() {
+  function getPokemons() {
     database.ref('pokemons').once('value', (snapshot) => {
       setPokemons(snapshot.val());
     });
@@ -33,47 +59,21 @@ function  GamePage () {
         
         if (pokemon.id === id) {
           pokemon.active = !pokemon.active;
-
-          database.ref(`pokemons/${key}`).set({
-            ...pokemon,
-            active: pokemon.active
-          });
         }
     
         acc[key] = pokemon;
+
+        database.ref(`pokemons/${key}`).set(pokemon);
+
         return acc;
       }, {})
     ));
   }
 
-  const addPokemon = () => {
-    const newKey = fire.database().ref().child('pokemons').push().key;
-    const data = {
-      abilities : [ "keen-eye", "tangled-feet", "big-pecks" ],
-      base_experience : 122,
-      height : 11,
-      id : newKey,
-      img : "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/17.png",
-      name : "pidgeotto",
-      stats : {
-        attack : 60,
-        defense : 55,
-        hp : 63,
-        'special-attack' : 50,
-        'special-defense' : 50,
-        speed : 71
-      },
-      type : "flying",
-      values : {
-        bottom : 7,
-        left : 5,
-        right : 2,
-        top : 'A'
-      }
-    };
-
-    fire.database().ref('pokemons/' + newKey).set(data);
-    getPokemonCards();
+  const handleAddPokemon = () => {
+    const data = DATA;
+    const newKey = database.ref().child('pokemons').push().key;
+    database.ref('pokemons/' + newKey).set(data).then(() => getPokemons());
   };
 
   return (
@@ -82,7 +82,7 @@ function  GamePage () {
         {/* <button onClick={handleClick}>&lt; Back to home page</button> */}
         <button 
           className={s.addNewPokemon}  
-          onClick={addPokemon}
+          onClick={handleAddPokemon}
         >Add new pokemon</button>
         <div className={s.flex}>
           {
